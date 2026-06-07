05aeac8d2fca6cbc9b437c88a1640826cb5dfe76
SetFodi <lukafartenadze2004@gmail.com>
2026-06-07 21:21:20 +0400

    Update website for v0.1.0-beta.2

--- lib/release.ts
+++ lib/release.ts
@@ -1,12 +1,12 @@
 // Single source of truth for the current AndSpace release.
 // Bump `version` + `tag` (and the asset name in `dmgUrl`) when you ship.
 export const RELEASE = {
-  version: "0.1.0-beta.1",
-  tag: "v0.1.0-beta.1",
+  version: "0.1.0-beta.2",
+  tag: "v0.1.0-beta.2",
   repoUrl: "https://github.com/SetFodi/Andspace",
   releasesUrl: "https://github.com/SetFodi/Andspace/releases",
-  tagUrl: "https://github.com/SetFodi/Andspace/releases/tag/v0.1.0-beta.1",
+  tagUrl: "https://github.com/SetFodi/Andspace/releases/tag/v0.1.0-beta.2",
   issuesUrl: "https://github.com/SetFodi/Andspace/issues",
   dmgUrl:
-    "https://github.com/SetFodi/Andspace/releases/download/v0.1.0-beta.1/AndSpace_0.1.0-beta.1_aarch64.dmg",
+    "https://github.com/SetFodi/Andspace/releases/download/v0.1.0-beta.2/AndSpace_0.1.0-beta.2_aarch64.dmg",
 } as const;
